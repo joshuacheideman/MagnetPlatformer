cc26af79468c1452cd1fcb860b7859f6a481321b
Joshua Heideman <jcheideman@ucdavis.edu>
2017-09-23 18:08:28 -0700

    Added Menu

---
+++ Assets/Joshua/Scripts/MenuManagement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuManagement : MonoBehaviour {
+
+	//Start Game
+	public void StartGame()
+	{
+		SceneManager.LoadScene (1);
+	}
+	//Quit out of Application
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+}
