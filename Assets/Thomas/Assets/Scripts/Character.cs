5115552437b0580f39749308a4bbbaaa07b43c7a
Thomas Tran <thomastran878@gmail.com>
2017-09-24 00:40:33 -0700

    Added basic movement to MagnetGuy

---
+++ Assets/Thomas/Assets/Scripts/Character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour {
+
+    [SerializeField]
+    protected float speed;
+    [SerializeField]
+    protected float jumpForce;
+
+    protected Rigidbody2D rb;
+    protected float horDirection;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+        Move();
+	}
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(horDirection * speed * Time.deltaTime, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+    }
+}
---
+++ Assets/Thomas/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character {
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+        GetInput();
+        base.Update();
+	}
+
+    void GetInput()
+    {
+        horDirection = Input.GetAxis("Horizontal");
+    }
+}
