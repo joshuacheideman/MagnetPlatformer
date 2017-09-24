59ba8b49b08cd88976340f82f21e0dc290d64237
Joshua Heideman <jcheideman@ucdavis.edu>
2017-09-24 01:04:56 -0700

    Modified Movement Script Slightly for Sprite Reversal
    Added line where it looks for sprite renderer trying to find component in child and just checks the bool depending on the value horizontal movement.

--- Assets/Thomas/Assets/Scripts/Player.cs
+++ Assets/Thomas/Assets/Scripts/Player.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : Character {
-
+	public SpriteRenderer spriterender; //Get MagnetGuys Sprite Renderer
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+		spriterender = this.gameObject.GetComponentInChildren<SpriteRenderer> ();
 	}
 	
 	// Update is called once per frame
@@ -18,5 +19,9 @@ public class Player : Character {
     void GetInput()
     {
         horDirection = Input.GetAxis("Horizontal");
+		if (horDirection > 0)
+			spriterender.flipX = false;
+		else if(horDirection < 0)
+			spriterender.flipX = true;
     }
 }
