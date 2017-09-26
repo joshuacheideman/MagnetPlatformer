3ae84c2ff3de9bfa3fa1fb3b3f333e7333b98f8f
Davey <daveybelliss@gmail.com>
2017-09-26 05:37:27 -0700

    Adding pushpin enemies

---
+++ Assets/Davey/Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Character {
+    [SerializeField]
+    protected int damage;
+    [SerializeField]
+    protected float detectionRange;
+
+    protected bool isGrounded = false;
+    protected bool isAttacking = false;
+    protected GameObject target;
+    // Use this for initialization
+    protected void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        Debug.Log("Initializng");
+        target = GameObject.FindGameObjectWithTag("Player");
+	}
+
+    virtual protected void FixedUpdate() {
+        if (!target) {
+            target = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+
+    protected Vector3 getVectorToTarget() {
+        if (target) {
+            Vector3 curPos = this.transform.position;
+            Vector3 targetPos = target.transform.position;
+            Vector3 toPlayer = targetPos - curPos;
+            float xdirection = toPlayer.x;
+            return toPlayer;
+        }
+        else {
+            return Vector3.zero;
+        }
+    }
+
+    virtual protected void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "Floor") {
+            isGrounded = true;
+        }
+    }
+}
---
+++ Assets/Davey/Assets/Scripts/MovingPushPin.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPushPin : Enemy {
+
+    [SerializeField]
+    protected float cooldown; // Cooldown of jump in seconds
+
+    private float curCooldown = 0;
+    // Update is called once per frame
+    override protected void FixedUpdate() {
+        base.FixedUpdate();
+        if (rb.velocity.y <= 0) {
+            render.flipY = true;
+        }
+        if (isGrounded) {
+            render.flipY = false;
+            isAttacking = false;
+        }
+
+        if (!isAttacking && curCooldown <= 0) {
+            if (target) {
+                Debug.Log("Finding target");
+                Vector3 toTarget = getVectorToTarget();
+                Debug.Log("Grounded:" + isGrounded);
+                Debug.Log(toTarget.x);
+                if (Mathf.Abs(toTarget.x) < detectionRange && isGrounded) {
+                    Debug.Log("Attack");
+                    rb.AddForce(Vector2.up * jumpForce);
+                    isGrounded = false;
+                    rb.AddForce(speed * Vector3.right);
+                    isAttacking = true;
+                    curCooldown = cooldown;
+                }
+                else {
+                    //idle();
+                }
+            }
+        }
+        else if (!isAttacking && curCooldown > 0) {
+            curCooldown -= Time.deltaTime;
+        }
+
+    }
+
+    override protected void OnCollisionEnter2D(Collision2D col) {
+        base.OnCollisionEnter2D(col);
+        if (col.gameObject.tag == "Player") {
+            Debug.Log("Ow, player took " + damage + " damage");
+            //col.gameObject.SendMessage("takeDamage", damage);
+        }
+    }
+}
