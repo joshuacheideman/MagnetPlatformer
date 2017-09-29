eb33cba292e96183b659527c020310254a2cdcf4
Joshua Heideman <jcheideman@ucdavis.edu>
2017-09-28 18:27:07 -0700

    Added block pushing and pulling for red and blue blocks

---
+++ Assets/Joshua/Scripts/BoxBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxBehavior : MonoBehaviour {
+	private GameObject Player;
+	float boxmovement;
+	void BoxControl(Transform Box,bool isPull)
+	{
+		float strength,offset;
+		Player = GameObject.FindGameObjectWithTag ("Player");
+		offset = Player.gameObject.transform.position.x - Box.position.x;
+		if (offset == 0.0f)//cannot divide by zero
+			return;
+		strength = 1 / offset;
+		if (offset < 0)
+			strength = -strength;
+		boxmovement = Mathf.Lerp (0, 1, strength/10);
+		if (isPull)
+			TranslateBoxes (Box, offset);
+		else
+			TranslateBoxes (Box, -offset);
+	}
+	public void BoxPull(Transform Box)
+	{
+		BoxControl (Box, true);
+	}
+	public void BoxPush(Transform Box)
+	{
+		BoxControl (Box,false);
+	}
+	void TranslateBoxes(Transform Box,float offset)
+	{
+		if (offset < 0)
+			Box.gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (-boxmovement * 100, 0.0f);
+		else if (offset > 0)
+			Box.gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (boxmovement * 100, 0.0f);
+		Debug.Log (boxmovement*100);
+		}
+}
--- Assets/Thomas/Assets/Scripts/Player.cs
+++ Assets/Thomas/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Player : Character {
 	// Use this for initialization
@@ -10,10 +11,13 @@ public class Player : Character {
 	bool inMagnetZone=false;//checks to see if above Metal blocks
 	bool JumpAtMaxHeight =false;
 	bool isHovering=false;
+	bool PullRed = true;//toggle for which boxes you pull or repel
 	private const int Idle = 0;
 	private const int JumpAnim =1;
 	private const int Crouch = 2;
 	private const int Moving =4;
+	RaycastHit2D[] AllBoxes = new RaycastHit2D[5];//amount of total red and blue boxes
+	BoxBehavior MoveBox;
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
 	}
@@ -21,11 +25,16 @@ public class Player : Character {
 	// Update is called once per frame
 	protected override void Update () {
 		Move ();
+		ToggleMagnet ();
+		//GetColliders ();
 		JumpAtMaxHeight=AtMaxHeight();
 		if (JumpAtMaxHeight)//check if past maxheight of jump to hover
 			Hover ();
 		else
 			isHovering = false;
+		GetColliders();
+		MagnetPowers ();
+		DeleteColliders ();
 	}
     void Move()
     {
@@ -35,6 +44,11 @@ public class Player : Character {
 		rb.velocity = new Vector2(horDirection * speed * Time.deltaTime, rb.velocity.y);
 		Jump ();
     }
+	void ToggleMagnet()
+	{
+		if (Input.GetButtonUp ("MagnetPull"))
+			PullRed = !PullRed;
+	}
 	void Flip()
 	{
 		if (horDirection > 0)
@@ -74,12 +88,54 @@ public class Player : Character {
 			rb.velocity = new Vector2 (rb.velocity.x, 0.0f);
 		}
 	}
+	void GetColliders()
+	{
+		Physics2D.CircleCastNonAlloc (transform.position, 14.0f, Vector2.right, AllBoxes, 1.0f, 1<<LayerMask.NameToLayer("MetalBlocks"));//do this to get all boxes that are in the circle
+	}
+	void DeleteColliders()//helper function to delete all colliders not with circlecast this frame.
+	{
+		Array.Clear (AllBoxes, 0, AllBoxes.Length);
+	}
 	bool AtMaxHeight()
 	{
-		if (!isGrounded && rb.velocity.y < 0)
+		if (!isGrounded && rb.velocity.y <= 0)
 			return true;
 		else
 			return false;
+	}
+	void MagnetPowers()
+	{
+		foreach (RaycastHit2D hit in AllBoxes) {
+			if (hit.collider == null)
+				break;
+			MoveBox = hit.collider.transform.gameObject.GetComponent<BoxBehavior> ();
+			if (!Input.GetButton ("MagnetPull")) {
+				Rigidbody2D BoxBody;
+				BoxBody = hit.collider.gameObject.GetComponent<Rigidbody2D> ();
+				BoxBody.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
+				BoxBody.velocity = new Vector2 (0.0f, 0.0f);
+			}
+			hit.collider.gameObject.GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezeRotation;
+			if (hit.collider.gameObject.GetComponentInParent<SpriteRenderer> ().color == Color.red) {
+				if(PullRed&&Input.GetButton("MagnetPull"))//this is the left ctrl key.
+				{
+					MoveBox.BoxPull (hit.collider.gameObject.transform);
+					Debug.Log("Pull red");
+				}
+				else if(!PullRed&&Input.GetButton("MagnetPull"))
+				{
+					Debug.Log("Push Red");
+					MoveBox.BoxPush (hit.collider.gameObject.transform);
+				}
+			}
+			if (hit.collider.gameObject.GetComponentInParent<SpriteRenderer> ().color == Color.blue) {
+				
+				if (!PullRed && Input.GetButton ("MagnetPull"))
+					MoveBox.BoxPull (hit.collider.gameObject.transform);
+				if (PullRed && Input.GetButton ("MagnetPull"))
+					MoveBox.BoxPush (hit.collider.gameObject.transform);
+			}
+		}
 	}
 	void OnCollisionEnter2D(Collision2D col)
 	{
@@ -87,12 +143,12 @@ public class Player : Character {
 	}
 	void OnTriggerStay2D(Collider2D col)
 	{
-		if (col.gameObject.name == "MagnetZone")
+		if (col.gameObject.name == "MagnetHoverZone")
 			inMagnetZone = true;
 	}
 	void OnTriggerExit2D(Collider2D col)
 	{
-		if (col.gameObject.name == "MagnetZone")
+		if (col.gameObject.name == "MagnetHoverZone")
 			inMagnetZone = false;
 	}
 }
