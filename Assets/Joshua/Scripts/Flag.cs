fad49a79253579566bd68d92a339ada2028d9b3b
Joshua Heideman <jcheideman@ucdavis.edu>
2017-09-29 03:06:17 -0700

    Added End of Level

---
+++ Assets/Joshua/Scripts/Flag.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Flag : MonoBehaviour {
+	Animator anim;
+	GameManager manager;
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponent<Animator> ();
+		manager = GameObject.Find ("GameManager").GetComponent<GameManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player") {
+			anim.SetBool ("RaiseFlag", true);
+			manager.SwitchScene (SceneManager.GetActiveScene ().buildIndex + 1);
+		}
+	}
+}
