1044844e9d3a9a697c1a10e69e7980c32df19de2
Davey <daveybelliss@gmail.com>
2017-09-29 00:01:51 -0700

    Plugman with attack animation

---
+++ Assets/Plugman.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plugman : Enemy {
+    [SerializeField]
+    protected Transform orbSpawn;
+    [SerializeField]
+    protected GameObject projectile;
+
+    // Update is called once per frame
+
+    // Use this for initialization
+    protected void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player");
+       
+    }
+
+    bool AnimatorIsPlaying() {
+        return anim.GetCurrentAnimatorStateInfo(0).length >
+               anim.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    }
+
+    bool AnimatorIsPlaying(string stateName) {
+        return AnimatorIsPlaying() && anim.GetCurrentAnimatorStateInfo(0).IsName(stateName);
+    }
+
+    void FixedUpdate () {
+        if (!AnimatorIsPlaying("AttackPlugman")) {
+
+            isAttacking = false;
+        }
+        if (target) {
+            Vector3 toTarget = getVectorToTarget();
+            if (toTarget.x < detectionRange && !isAttacking) {
+                anim.SetTrigger("isAttacking");
+                Instantiate(projectile, orbSpawn, true);
+                isAttacking = true;
+            }
+        }
+	}
+
+}
