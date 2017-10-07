dee5753ad5e4094d9a95c49d04c1bfa1fa0901f2
dbelliss <daveybelliss@yahoo.com>
2017-10-06 18:28:36 -0700

    Adding updates to electric orb

---
+++ Assets/Davey/Assets/Scripts/ElectricOrb.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricOrb :  Projectile {
+
+	override protected void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.tag == "Floor" && isDestroyedOnWall) {
+			Destroy(this.gameObject);
+		}
+		if (col.gameObject.tag == "Player") {
+			onHitPlayer();
+			Destroy(this.gameObject);
+		}
+	}
+
+	protected void Start() {
+		startHelper();
+	}
+
+	protected void startHelper() {
+		StartCoroutine (expire ());
+		rb = GetComponent<Rigidbody2D>();
+		target = GameObject.FindGameObjectWithTag("Player");
+		Vector3 forceVector = getVectorToTarget ().normalized * speed;
+		rb.AddForce(forceVector);
+	}
+
+	virtual protected void onHitPlayer() {
+		Debug.Log ("Player was hit");
+	}
+
+
+	protected void Update() {
+		this.transform.Rotate(Vector3.forward * Time.deltaTime * 5, Space.World);
+	}
+		
+
+	IEnumerator expire() {
+		blink ();
+		yield return new WaitForSeconds(duration - 1);
+		StartCoroutine (blink ());
+		yield return new WaitForSeconds(1);
+		Debug.Log ("Expiring");
+		Destroy (this.gameObject);
+	}
+
+	 
+
+
+	IEnumerator blink() {
+		SpriteRenderer spr = GetComponent<SpriteRenderer>();
+		while (true) {
+			spr.enabled = false;
+			yield return new WaitForSeconds (.1f);
+			spr.enabled = true;
+			yield return new WaitForSeconds (.1f);
+		}
+
+	}
+
+}
---
+++ Assets/Davey/Assets/Scripts/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Projectile : MonoBehaviour {
+    [SerializeField]
+    protected int damage;
+    [SerializeField]
+    protected float speed;
+	[SerializeField]
+	protected float duration;
+	[SerializeField]
+	protected bool isDestroyedOnWall;
+
+    protected Rigidbody2D rb;
+    protected GameObject target;
+
+
+	abstract protected void OnCollisionEnter2D (Collision2D col);
+
+    protected Vector3 getVectorToTarget() {
+        if (target) {
+            Vector3 curPos = this.transform.position;
+            Vector3 targetPos = target.transform.position;
+            Vector3 toPlayer = targetPos - curPos;
+            float xdirection = toPlayer.x;
+            return toPlayer;
+        }
+        else {
+            return Vector3.zero;
+        }
+    }
+}
